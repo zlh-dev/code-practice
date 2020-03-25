7f3bac283a5764f89f797a19c9038cbad8877544
zlh-dev <zhanglh30@gmail.com>
2020-03-25 21:37:11 +0800

    项目搭建

---
+++ springboot-mybatisplus/src/main/java/cn/code/springbootmybatisplus/SpringbootMybatisplusApplication.java
@@ -0,0 +1,16 @@
+package cn.code.springbootmybatisplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 张灵辉
+ */
+@SpringBootApplication
+public class SpringbootMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisplusApplication.class, args);
+    }
+
+}

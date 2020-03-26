264645b2240630810f1a083327c36400045b8ef0
zlh-dev <zhanglh30@gmail.com>
2020-03-26 22:02:31 +0800

    配置mybatis-plus

--- springboot-mybatisplus/src/main/java/cn/code/springbootmybatisplus/SpringbootMybatisplusApplication.java
+++ springboot-mybatisplus/src/main/java/cn/code/springbootmybatisplus/SpringbootMybatisplusApplication.java
@@ -1,5 +1,6 @@
 package cn.code.springbootmybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author 张灵辉
  */
 @SpringBootApplication
+@MapperScan("cn.code.springbootmybatisplus.mapper")
 public class SpringbootMybatisplusApplication {
 
     public static void main(String[] args) {

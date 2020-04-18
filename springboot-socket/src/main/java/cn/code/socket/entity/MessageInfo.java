ae201d926b60ee856bd2a9d201c387a56b62ac31
zlh-dev <zhanglh30@gmail.com>
2020-04-18 17:33:28 +0800

    socket初版完成

---
+++ springboot-socket/src/main/java/cn/code/socket/entity/MessageInfo.java
@@ -0,0 +1,20 @@
+package cn.code.socket.entity;
+
+
+import lombok.Data;
+
+/**
+ * 接收前台用户信息类
+ *
+ * @author zlh-dev
+ * @date 2020/04/18 17:14
+ */
+@Data
+public class MessageInfo {
+
+    /**
+     * 消息内容
+     */
+    String msgContent;
+
+}

de3526e0bca763e5bd67620bfedc4817c0938b6c
zlh-dev <zhanglh30@gmail.com>
2020-04-27 21:21:24 +0800

    fix(配置文件): 加密
    配置文件加密和示例代码完善

--- springboot-mybatisplus/src/test/java/cn/code/mybatisplus/MybatisplusApplicationTests.java
+++ springboot-mybatisplus/src/test/java/cn/code/mybatisplus/MybatisplusApplicationTests.java
@@ -5,13 +5,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MybatisplusApplicationTests {
-
+    @Autowired
+    private StringEncryptor encryptor;
     @Autowired
     private ProvincesService provincesService;
 
@@ -23,4 +26,19 @@ class MybatisplusApplicationTests {
         System.out.println(json);
     }
 
+    /**
+     * 加密示例
+     */
+    @Test
+    void encrypt() {
+        String url = encryptor.encrypt("jdbc:mysql://localhost:3306/mysql?autoReconnect=true&serverTimezone=GMT%2B8&useUnicode=true&characterEncoding=utf-8");
+        String name = encryptor.encrypt("root");
+        String password = encryptor.encrypt("123456");
+        System.out.println("database url: " + url);
+        System.out.println("database name: " + name);
+        System.out.println("database password: " + password);
+        Assert.assertTrue(url.length() > 0);
+        Assert.assertTrue(name.length() > 0);
+        Assert.assertTrue(password.length() > 0);
+    }
 }

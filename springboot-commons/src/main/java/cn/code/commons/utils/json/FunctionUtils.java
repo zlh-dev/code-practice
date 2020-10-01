ffe8652fb35a7adfb06858a3c94edb106b4f9398
王亚旭 <wangyaxualy@aliyun.com>
2020-10-01 17:39:29 +0800

    测试提交代码

---
+++ springboot-commons/src/main/java/cn/code/commons/utils/json/FunctionUtils.java
@@ -0,0 +1,15 @@
+package cn.code.commons.utils.json;
+
+
+/**
+ * function 工具类
+ *
+ * @author wyx
+ * @date 2020/10/01
+ */
+public class FunctionUtils {
+
+    public static String testPush() {
+        return null;
+    }
+}
